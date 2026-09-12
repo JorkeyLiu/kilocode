78955806ad5735c8e6f39af68bc39b5b50f40f9f
Jorkey Liu <jorkeyliu@gmail.com>
2026-09-12 19:22:12 +0800

    test(cli): use an unknown fd carrier method
    Signed-off-by: Jorkey Liu <jorkeyliu@gmail.com>

--- packages/opencode/test/kilocode/server/fd-carrier-path.test.ts
+++ packages/opencode/test/kilocode/server/fd-carrier-path.test.ts
@@ -397,7 +397,7 @@ describe("fd-carrier path/get (parity-only read)", () => {
         try {
           yield* Effect.promise(() => init(ext))
           const err = yield* Effect.promise(() =>
-            ext.request("session/abort", pathReq(dir)).then(
+            ext.request("session/unknown-probe", pathReq(dir)).then(
               () => undefined,
               (e: unknown) => e,
             ),
